b04c8fefb7a61f6ff7b9bfa57420ba1650b94190
Lorenzo <lperotti97@gmail.com>
2025-09-30 16:22:26 +0200

    fixes vari

--- app/team/page.js
+++ app/team/page.js
@@ -3,7 +3,7 @@ import Card from "../components/Card";
 import "./page.css";
 
 export default function Team() {
-  let cards = [
+  const cards = [
     {
       src: "https://picsum.photos/200",
       alt: "Image number 1",
@@ -11,19 +11,19 @@ export default function Team() {
       text: "Description for image 1",
     },
     {
-      src: "https://picsum.photos/200",
+      src: "https://picsum.photos/201",
       alt: "Image number 2",
       title: "Image 2",
       text: "Description for image 2",
     },
     {
-      src: "https://picsum.photos/200",
+      src: "https://picsum.photos/202",
       alt: "Image number 3",
       title: "Image 3",
       text: "Description for image 3",
     },
     {
-      src: "https://picsum.photos/200",
+      src: "https://picsum.photos/203",
       alt: "Image number 4",
       title: "Image 4",
       text: "Description for image 4",
